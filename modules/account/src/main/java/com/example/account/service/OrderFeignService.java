11c36341096efba5dfe3687b159518246df272fb
wangyu <wangyu_eva@163.com>
2019-04-08 16:45:36 +0800

    update

--- modules/account/src/main/java/com/example/account/service/OrderFeignService.java
+++ modules/account/src/main/java/com/example/account/service/OrderFeignService.java
@@ -7,8 +7,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
  * @author wangyu
  * 2019/01/14 23:13
  */
-@FeignClient(serviceId="order")
+@FeignClient(serviceId = "order")
 public interface OrderFeignService {
+    /**
+     * feign测试
+     *
+     * @return hello String
+     */
     @RequestMapping("home/hello")
     String hello();
 }
